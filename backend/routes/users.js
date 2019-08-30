0cd01a7e04f7bd8d7a699e7846ad6a896a717f72
Patryk <patryk-romaniuk@o2.pl>
2019-08-30 13:27:03 +0200

    Made validation by User Schema

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,6 +13,26 @@ router.post(
         check('username',"Username is required").not().isEmpty()
     ],
     async(req,res) => {
-        
+        const { name,last_name,email,password,username } = req.body;
+        let validateUserByEmail = await User.findOne({ email });
+        let validateUserByUserName = await User.findOne({ username });
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
+        if(validateUserByEmail){
+            return res.status(401).json({ msg: "There is already user with this Email" });
+        }
+        if(validateUserByUserName){
+            return res.status(401).json({ msg: "There is already user with this Username" });
+        }
+        try {
+            
+        } catch (error) {
+            console.log(error.message);
+            return res.status(500).json({ msg: "Server Error..." });
+        }
     }
-)
+);
+
+module.exports = router;
