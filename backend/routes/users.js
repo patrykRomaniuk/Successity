2e0b01a4b0a6e64f8bbf5c29244ccad59cd18862
Patryk <patryk-romaniuk@o2.pl>
2019-08-31 12:27:56 +0200

    Fixed order and added username to User object

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -12,12 +12,13 @@ router.post(
     [
         check('name','Name is required').not().isEmpty(),
         check('last_name','Last Name is required').not().isEmpty(),
+        check('username',"Username is required").not().isEmpty(),
         check('email','Email is required').isEmail(),
-        check('password',"Password is required").not().isEmpty(),
-        check('username',"Username is required").not().isEmpty()
+        check('password',"Password is required").not().isEmpty()
     ],
     async(req,res) => {
-        const { name,last_name,email,password,username } = req.body;
+        const { name,last_name,username,email,password } = req.body;
+        console.log(req.body);
         let validateUserByEmail = await User.findOne({ email });
         let validateUserByUserName = await User.findOne({ username });
         const errors = validationResult(req);
@@ -39,6 +40,7 @@ router.post(
             user = new User({
                 name,
                 email,
+                username,
                 last_name,
                 password,
                 avatar
