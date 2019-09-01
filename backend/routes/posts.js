7783ac9225c6b3a288056a123df1dc0e105c1a70
Patryk <patryk-romaniuk@o2.pl>
2019-09-01 10:09:07 +0200

    Added post make feature

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const auth = require('../middleware/auth');
 const router = express.Router();
+const User = require('../modules/User');
+const Post = require('../modules/Post');
+const { validationResult,check } = require('express-validator');
 
 router.post(
     '/',
+    [
+        check('text','Text is required').not().isEmpty()
+    ],
     auth,
     async(req,res) => {
+        const { text } = req.body;
+        const errors = validationResult(req);
+        let user = await User.findById(req.user.id).select('-password');
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
+
         try {
-            
+            let post = new Post({
+                text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            });
+            const registeredPost = await post.save();
+            res.json(registeredPost);
         } catch (error) {
             console.log(error.message);
             return res.status(500).json({ msg: "Server Error..." });
