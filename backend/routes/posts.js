0e1bf1dce1b477dc090f76d48f248cb4e23dd174
Patryk <patryk-romaniuk@o2.pl>
2019-09-01 18:59:46 +0200

    Added features of adding likes and comments

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -60,6 +60,78 @@ router.post(
             return res.status(500).json({ msg: "Server Error..." });
         }
     }
+);
+
+router.put(
+    '/likes/:post_id',
+    auth,
+    async(req,res) => {
+        try {
+            let post = await Post.findById(req.params.post_id);
+            if(!post){
+                return res.status(401).json({ msg: "There is not post that, you want like" });
+            }
+            if(post.likes.filter(like => like.user.toString()).length > 0){
+                return res.status(400).json({ msg: "Post Already Liked" });
+            }
+            post.likes.unshift({ user: req.user.id });
+            await post.save();
+            res.json(post);
+        } catch (error) {
+            console.log(error.message);
+            return res.status(500).json({ msg: "Server Error..." });
+        }
+    }
+);
+
+router.put(
+    '/comments/:post_id',
+    [
+        check('text',"Text is required").not().isEmpty()
+    ],
+    auth,
+    async(req,res) => {
+        const { text } = req.body;
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            let post = await Post.findById(req.params.post_id);
+            let user = await User.findById(req.user.id);
+            let comment = {
+                text,
+                name: user.name,
+                avatar: user.avatar
+            };
+            post.comments.unshift(comment);
+            await post.save();
+            res.json(post);
+        } catch (error) {
+            console.log(error.message);
+            return res.status(500).json({ msg: "Server Error..." });
+        }
+    }
+)
+
+router.delete(
+    '/:post_id',
+    auth,
+    async(req,res) => {
+        try {
+            let post = await Post.findById(req.params.post_id);
+            if(post.user.toString() !== req.user.id){
+                return res.status(500).json({ msg: "You are not allowed to do that" });
+            }
+            await post.remove();
+            res.json({ msg: "Post removed" });
+        } catch (error) {
+            console.log(error.message);
+            return res.status(500).json({ msg: "Server Error..." });
+        }
+    }
 )
 
 module.exports = router;
