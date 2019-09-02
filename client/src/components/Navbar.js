21cd79a4dfac7b981710dd6cf74690ed1f938f70
Patryk <patryk-romaniuk@o2.pl>
2019-09-02 18:22:45 +0200

    Added classnames

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className="main__nav">
+            <div className="logo-wrapper">LOGO</div>
+            <div className="nav-links">
+                <a href="#" className="href__style__remove nav__link">Users</a>
+                <a href="#" className="href__style__remove nav__link">Topics</a>
+                <a href="#" className="href__style__remove nav__link">Login</a>
+                <a href="#" className="href__style__remove nav__link">Sign Up</a>
+            </div>
+        </nav>
     )
 }
 
