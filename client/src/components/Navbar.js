a7168b1de61cfe76b6cf10b464a530cb99911708
Patryk <patryk-romaniuk@o2.pl>
2019-09-02 18:49:04 +0200

    Changed hrefs to 'to'

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,9 +7,9 @@ const Navbar = () => {
             <div className="logo-wrapper">LOGO</div>
             <div className="nav-links">
                 <Link to="/users" className="href__style__remove nav__link">Users</Link>
-                <Link href="#" className="href__style__remove nav__link">Topics</Link>
-                <Link href="#" className="href__style__remove nav__link">Login</Link>
-                <Link href="#" className="href__style__remove nav__link">Sign Up</Link>
+                <Link to="/topics" className="href__style__remove nav__link">Topics</Link>
+                <Link to="/login" className="href__style__remove nav__link">Login</Link>
+                <Link to="/register" className="href__style__remove nav__link">Sign Up</Link>
             </div>
         </nav>
     )
