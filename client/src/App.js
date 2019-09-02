c85221029ba3ced43e0b46ca199599279a303b4d
Patryk <patryk-romaniuk@o2.pl>
2019-09-02 17:45:27 +0200

    Added styling to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
 import Landing from './pages/Landing';
+import './styles/App.css';
 
 const App = () => {
   return (
