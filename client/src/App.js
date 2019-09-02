f52058fa11966ed5868139c7ac74faa3934f854f
Patryk <patryk-romaniuk@o2.pl>
2019-09-02 17:39:19 +0200

    Initialized Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import Landing from './pages/Landing';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+       <Route path="/" component={ Landing } />
+      </Switch>
+    </Router>
   )
 }
 
