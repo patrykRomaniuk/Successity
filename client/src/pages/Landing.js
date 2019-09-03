6c6303c47d2581a2d633ccc6c5ad3e83f66e605e
Patryk <patryk-romaniuk@o2.pl>
2019-09-03 17:44:29 +0200

    Made minor changes

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
     return (
-        <div>
-           <Navbar/> 
+        <div className="landing-wrapper">
+            <div className="text-wrapper">
+                <div className="text-header-wrapper">
+                    <p className="text-header font__p p__size">Witaj w</p>
+                    <span>Chatglass {' '}</span> 
+                </div>
+
+                <div className="text-section font__p p__size">
+                    Jest to forum stripte poświęcone tematyce naszego wzroku.
+                    <br/>
+                    Jeśli szukasz odpowiedzi na pytania dotyczące:
+                    <ul>
+                        <li> chorób oczu</li>
+                        <li> wad wzroku</li>
+                        <li> dobóru okularów</li>
+                        <li> lub innych tego typu problemów</li>
+                    </ul>
+                    <div className="text-button-wrapper">
+                        <Link to="/register">
+                            Zarejestruj się
+                        </Link>
+                       {' '}  i zadaj pytanie!
+                    </div>
+                </div>
+
+            </div>
+            <div className="image-wrapper">
+
+            </div>
         </div>
     )
 }
