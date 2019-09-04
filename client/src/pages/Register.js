e541f75f30f01cdd5ece673263c4474ebde6690c
Patryk <patryk-romaniuk@o2.pl>
2019-09-04 20:52:07 +0200

    Made all html of Register Page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,105 @@
-import React from 'react'
+import React,{ useState } from 'react';
+import '../styles/RegisterPage.css';
 
 const Register = () => {
+
+    const [ formData,setFormData ] = useState({
+        name: '',
+        last_name: '',
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    const { name,last_name,username,email,password } = formData;
+
+    const onChange = e => {
+        setFormData({ ...formData,[e.target.name]: e.target.value })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log('ahah')
+        setFormData({ ...formData,password: '' })
+    }
+
     return (
-        <div>
-            Register
-        </div>
+        <main className="register-page-wrapper">
+            <form className="register-section" onSubmit={(e) => onSubmit(e)}> 
+                <div className="inputs-wrapper">
+
+                    <header className="register-header-wrapper">
+                        <p className="font__p p__size register-header">
+                            <i className="fas fa-users users-icon app_color_font"></i>
+                            Zarejestruj się
+                        </p>
+                    </header>
+
+                    <div className="label-wrapper">
+                        <label className="label__register p__size">Imię</label>
+                    </div>
+
+                    <input 
+                    type="text"
+                    name="name"
+                    value={ name }
+                    onChange={(e) => onChange(e)}
+                    />
+
+                    <div className="label-wrapper">
+                        <label className="label__register p__size">Nazwisko</label>
+                    </div>
+                    <input 
+                    type="text"
+                    value={ last_name }
+                    name="last_name"
+                    onChange={(e) => onChange(e)}
+                    />
+
+                    <div className="label-wrapper">
+                        <label className="label__register p__size">Pseudonim</label>
+                    </div>
+                    <input
+                    type="text"
+                    name="username"
+                    value={ username }
+                    onChange={(e) => onChange(e)}
+                    />
+
+                    <div className="label-wrapper">
+                        <label className="label__register p__size">E-mail</label>
+                    </div>
+                    <input 
+                    name="email"
+                    value={ email }
+                    type="email"
+                    onChange={(e) => onChange(e)}
+                    />
+
+                    <div className="label-wrapper">
+                        <label className="label__register p__size">Hasło</label>
+                    </div>
+                    <input 
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => onChange(e)}
+                    />
+
+                    <div className="label-wrapper">
+                        <p className="p__size font__p password__info">
+                            <i className="fas fa-user-check app_color_font"></i>{' '}
+                             Hasło musi mieć co najmniej 6 liter
+                        </p>
+                    </div>
+
+                    <div className="button-wrapper app_color_background" onClick={(e) => onSubmit(e)}>
+                        <p className="button-letter">Zarejestruj się</p>
+                    </div>
+
+                </div>
+            </form>
+        </main>
     )
 }
 
