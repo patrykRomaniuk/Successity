8209ec10406f4a5826ba2dbc88206da3e0ac2bfe
Patryk <patryk-romaniuk@o2.pl>
2019-09-04 21:07:03 +0200

    Changed initialState

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,12 @@
 
-const initialState = [];
+const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: false,
+    isLoading: false,
+    user: {},
+    users: {},
+    errors: {}
+};
 
 const auth = (state = initialState, action) => {
     const { type,payload } = action;
