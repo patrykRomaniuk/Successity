ec21ba6c5cff066900d6cc768f2918fcaaecd0dd
Patryk <patryk-romaniuk@o2.pl>
2019-09-04 17:08:33 +0200

    Added register route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Navbar from './components/Navbar';
+import Register from './pages/Register';
 import './styles/App.css';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar/> 
       <Switch>
        <Route exact path="/" component={ Landing } />
+       <Route exact path="/register" component={ Register }/>
       </Switch>
     </Router>
   )
