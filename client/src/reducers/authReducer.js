d8897100c5315e35f6e9eea39365734c212eb4d0
Patryk <patryk-romaniuk@o2.pl>
2019-09-04 20:58:35 +0200

    Added rootReducer and authReducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+
+const initialState = [];
+
+const auth = (state = initialState, action) => {
+    const { type,payload } = action;
+    switch(type){
+        default:
+            return state;
+    }
+}
+
+export default auth;
