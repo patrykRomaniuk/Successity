d351b8919ffb381d179a31f44e4be3785ecb4098
Patryk <patryk-romaniuk@o2.pl>
2019-09-04 21:01:06 +0200

    Made Provider

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Navbar from './components/Navbar';
 import Register from './pages/Register';
+import { Provider } from 'react-redux';
+import store from './store';
 import './styles/App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Navbar/> 
-      <Switch>
-       <Route exact path="/" component={ Landing } />
-       <Route exact path="/register" component={ Register }/>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar/> 
+        <Switch>
+        <Route exact path="/" component={ Landing } />
+        <Route exact path="/register" component={ Register }/>
+        </Switch>
+      </Router>
+    </Provider>
   )
 }
 
