53a36bc93c42d16c4dadd7cd4f405a6eb5cc583b
Patryk <patryk-romaniuk@o2.pl>
2019-09-05 16:35:28 +0200

    Made dispatch handlers

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,8 +1,16 @@
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    LOGIN_FAIL,
+    LOGIN_SUCCESS,
+    AUTH_ERROR,
+    USER_LOADED
+} from '../actions/constants';
 
 const initialState = {
     token: localStorage.getItem('token'),
     isAuthenticated: false,
-    isLoading: false,
+    isLoading: true,
     user: {},
     users: {},
     errors: {}
@@ -11,6 +19,34 @@ const initialState = {
 const auth = (state = initialState, action) => {
     const { type,payload } = action;
     switch(type){
+        case USER_LOADED:
+            localStorage.getItem('token');
+            return {
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+                isLoading: false,
+                user: payload
+            }
+        case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
+            localStorage.setItem('token',payload.token);
+            return {
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+                isLoading: false
+            }
+        case AUTH_ERROR:
+        case LOGIN_FAIL:
+        case REGISTER_FAIL:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                ...payload,
+                isAuthenticated: false,
+                isLoading: true
+            }
         default:
             return state;
     }
