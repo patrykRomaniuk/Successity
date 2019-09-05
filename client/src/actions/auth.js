1bc8692555381daa8a991d3a8929c3639715fd01
Patryk <patryk-romaniuk@o2.pl>
2019-09-05 18:47:46 +0200

    Added loadUser to log out

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -77,6 +77,7 @@ export const logOut = () => async dispatch => {
         dispatch({
             type: LOG_OUT
         });
+        dispatch(loadUser());
     } catch (error) {
         console.log(error.message);
     }
