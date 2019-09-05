9c1aea8d56ed13e499f5ccaa0c7deaf31d8da99f
Patryk <patryk-romaniuk@o2.pl>
2019-09-05 15:15:13 +0200

    Made register,login and get user features

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,12 +1,29 @@
 import { 
     REGISTER_SUCCESS,
     REGISTER_FAIL,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
     AUTH_ERROR,
     USER_LOADED 
 } from './constants';
 import axios from 'axios';
 
-export const registerUser = ( name,last_name,username,email,password ) => dispatch => {
+export const loadUser = () => async dispatch => {
+    try {
+        const res = await axios.get('http://localhost:5000/api/users');
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        });
+    } catch (error) {
+        console.log(error.message);
+        dispatch({
+            type: AUTH_ERROR
+        });
+    }
+}
+
+export const registerUser = ( name,last_name,username,email,password ) => async dispatch => {
     try {
         const body = JSON.stringify({ name,last_name,username,email,password });
         const config = {
@@ -17,10 +34,31 @@ export const registerUser = ( name,last_name,username,email,password ) => dispat
             type: REGISTER_SUCCESS,
             payload: res.data
         });
+        dispatch(loadUser());
     } catch (error) {
         console.log(error.message);
         dispatch({
             type: REGISTER_FAIL
+        });
+    }
+}
+
+export const loginUser = ( email,password ) => async dispatch => {
+    try {
+        const config = {
+            'Content-Type': 'application/json'
+        };
+        const body = JSON.stringify({ email,password });
+        const res = await axios.post('http://localhost:5000/api/users/login',body,config);
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+        dispatch(loadUser());
+    } catch (error) {
+        console.log(error.message);
+        dispatch({
+            type: LOGIN_FAIL
         });
     }
 }
