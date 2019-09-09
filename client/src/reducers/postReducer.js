97ee1d6c76557a9b15e194aaf69716ce99ed610e
Patryk <patryk-romaniuk@o2.pl>
2019-09-09 08:35:38 +0200

    Added POST_CLEAR action

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,51 @@
+import {
+    MAKE_POST,
+    POST_ERROR,
+    GET_USER_POSTS,
+    GET_POSTS,
+    GET_POST,
+    REMOVE_POST,
+    POST_CLEAR
+} from '../actions/constants'
+
+const initialState = {
+    posts: [],
+    post: null,
+    isLoading: true,
+    errors: {}
+}
+
+const post = (state = initialState,action) => {
+    const { type,payload } = action;
+    switch(type){
+        case POST_CLEAR:
+            return {
+                ...state,
+                post: null
+            }
+        case MAKE_POST:
+        case GET_POST:
+        case GET_USER_POSTS:
+            return {
+                ...state,
+                isLoading: false,
+                post: payload
+            }
+        case GET_POSTS:
+            return {
+                ...state,
+                isLoading: false,
+                posts: payload
+            }
+        case POST_ERROR:
+            return {
+                ...state,
+                isLoading: false,
+                errors: payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default post;
