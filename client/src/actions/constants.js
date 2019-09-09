794d6a02a9d23da32b8761e1359801cf638d3604
Patryk <patryk-romaniuk@o2.pl>
2019-09-09 08:34:55 +0200

    Added POST_CLEAR constant

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -5,3 +5,12 @@ export const LOGIN_FAIL = "LOGIN_FAIL";
 export const USER_LOADED = "USER_LOADED";
 export const AUTH_ERROR = "AUTH_ERROR";
 export const LOG_OUT = "LOG_OUT";
+
+export const MAKE_POST = "MAKE_POST";
+export const POST_ERROR = "POST_ERROR";
+export const GET_USER_POSTS = "GET_USER_POSTS";
+export const REMOVE_POST = "REMOVE_POST";
+export const GET_POSTS = "GET_POSTS";
+export const GET_POST = "GET_POST";
+
+export const POST_CLEAR = "POST_CLEAR";
