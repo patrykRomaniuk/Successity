aec6199500a784bc39add478a13703bb3c1ea21c
Patryk <patryk-romaniuk@o2.pl>
2019-09-13 21:50:28 +0200

    Added ADD_LIKE constant

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -5,13 +5,14 @@ export const LOGIN_FAIL = "LOGIN_FAIL";
 export const USER_LOADED = "USER_LOADED";
 export const AUTH_ERROR = "AUTH_ERROR";
 export const LOG_OUT = "LOG_OUT";
-
 export const MAKE_POST = "MAKE_POST";
 export const POST_ERROR = "POST_ERROR";
 export const GET_USER_POSTS = "GET_USER_POSTS";
 export const REMOVE_POST = "REMOVE_POST";
 export const GET_POSTS = "GET_POSTS";
 export const GET_POST = "GET_POST";
-
 export const POST_CLEAR = "POST_CLEAR";
 export const SEARCH_TOPICS = "SEARCH_TOPICS";
+export const GET_USERS = "GET_USERS";
+export const MOST_LIKED_POSTS = "MOST_LIKED_POSTS";
+export const ADD_LIKE = "ADD_LIKE";
