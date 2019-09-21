e42338c48e8caa28d84b780c93bf847f66ff885e
Patryk <patryk-romaniuk@o2.pl>
2019-09-21 11:54:46 +0200

    Added get posts action request to props in wrapper

--- client/src/pages/LoopHandler/TopicPostsWrapper.js
+++ client/src/pages/LoopHandler/TopicPostsWrapper.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import TopicPost from './TopicPost';
 
-const UserPostsWrapper = ({ posts }) => 
+const UserPostsWrapper = ({ 
+    posts,
+    isOldest,
+    isLatest,
+    isMostCommented,
+    isMostLikedPosts 
+}) => 
     posts !== null &&
     posts.length > 0 &&
-    posts.map(post => <TopicPost post={post} key={post._id}/>)
+    posts.map(post => <TopicPost
+        isOldest={ isOldest }
+        isLatest={ isLatest }
+        isMostCommented={ isMostCommented }
+        isMostLikedPosts={ isMostLikedPosts }
+         post={post} 
+         key={post._id}
+         />)
 
 export default UserPostsWrapper;
