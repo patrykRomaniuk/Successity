912797752c6b249de961aaba9e93f8d1d4659439
Patryk <patryk-romaniuk@o2.pl>
2019-09-29 10:48:06 +0200

    Changed classNamesc

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -1,18 +1,64 @@
-import React,{ useEffect } from 'react';
+import React,{ useEffect,useState } from 'react';
 import { connect } from 'react-redux';
-import { getUsers } from '../actions/auth';
+import { getUsers,searchUsers } from '../actions/users';
+import UsersWrapper from './LoopHandler/UsersWrapper';
 
-const Users = ({ getUsers }) => {
+const Users = ({ getUsers,searchUsers,auth }) => {
+
     useEffect(() => {
         getUsers();
     },[])
+
+    const [ searchValue,setSearchValue ] = useState('');
+
+    const onChange = e => {
+        setSearchValue(e.target.value);
+    }
+
+    const checkSearch = searchValue => {
+        if(searchValue !== '' && searchValue !== null){
+            searchUsers(searchValue);
+        } else {
+            getUsers();
+        }
+    }
+
     return (
         <div>
-            <div className="users-page-header">
-                Users Page
+            <header className="users-header">
+                <p className="app_color_font font__bold font__p users-headline">
+                    Users
+                </p>
+                <br/>
+
+                <form onSubmit={() => {
+                    checkSearch(searchValue)
+                }} className="search-user-wrapper">
+
+                 <textarea 
+                 value={ searchValue }
+                 onChange={(e) => onChange(e)}
+                 type="text"
+                 />
+
+                 <div className="user-search-button app_color_background font__p font__bold" onClick={() => {
+                    checkSearch(searchValue)
+                }}>
+                     Search for user
+                 </div>
+
+                </form>
+            </header>
+
+            <div className="users-wrapper">
+                <UsersWrapper users={auth.users}/>
             </div>
         </div>
     )
 }
 
-export default connect(null, { getUsers })(Users);
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, { getUsers,searchUsers })(Users);
