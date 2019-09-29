c1793a0236b5609bfe607def42d645349b0a0d01
Patryk <patryk-romaniuk@o2.pl>
2019-09-29 10:48:39 +0200

    Added User reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './authReducer';
+import user from './userReducer';
 import post from './postReducer';
 
 export default combineReducers({
     auth,
+    user,
     post
 });
