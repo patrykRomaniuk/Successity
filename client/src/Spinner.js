b9b1978c03fe77e0e138775340eee353a8b48323
Patryk <patryk-romaniuk@o2.pl>
2019-10-02 18:17:57 +0200

    Removed brackets

--- client/src/Spinner.js
+++ client/src/Spinner.js
@@ -2,15 +2,14 @@ import React from 'react';
 import Loader from 'react-loader-spinner'
 
 const Spinner = () => {
-    return (
-            <Loader
+    return  <Loader
             type="ThreeDots"
             color="#00BFFF"
             height={100}
             width={100}
             timeout={3000}
              />
-    )
+    
 }
 
 export default Spinner
