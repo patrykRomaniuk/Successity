5f9266fc6c4c0173d4d11a290aba926060d50e5f
Patryk <patryk-romaniuk@o2.pl>
2019-10-15 16:06:20 +0200

    Added CHANGE_REJECT constant

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -29,3 +29,4 @@ export const CHANGE_EMAIL = "CHANGE_EMAIL";
 export const CHANGE_USERNAME = "CHANGE_USERNAME";
 export const CHANGE_LAST_NAME = "CHANGE_LAST_NAME";
 export const CHANGE_NAME = "CHANGE_NAME";
+export const CHANGE_REJECT = "CHANGE_REJECT";
