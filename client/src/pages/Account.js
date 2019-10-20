6557782ea0ed52d257ee5a78c81d69c55a7dfdbd
Patryk <patryk-romaniuk@o2.pl>
2019-10-20 15:22:12 +0200

    Removed redundant usestate in action page

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,4 +1,4 @@
-import React,{ useEffect,useState } from 'react';
+import React,{ useEffect } from 'react';
 import { getUserPosts } from '../actions/users';
 import '../styles/Account.css';
 import { Redirect } from 'react-router-dom';
