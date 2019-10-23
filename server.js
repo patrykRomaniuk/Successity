60bf4489bfe3487880085f162a467ba62a913570
Patryk <patryk-romaniuk@o2.pl>
2019-10-23 17:20:37 +0200

    Prepared to deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const connectDB = require('./config/db');
 const cors = require('cors');
@@ -13,6 +14,14 @@ app.use(bodyParser.json());
 
 app.use('/api/posts',require('./routes/posts'));
 app.use('/api/users',require('./routes/users'));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
