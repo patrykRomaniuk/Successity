af165499e640c1ff45dc6cba51a1d5fd80852612
Patryk <patryk-romaniuk@o2.pl>
2019-10-26 15:53:09 +0200

    Uncommented api routes

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(cors());
 
 app.use(express.json({ extended: false }));
 
-//app.use('/api/posts',require('./routes/posts'));
-//app.use('/api/users',require('./routes/users'));
+app.use('/api/posts',require('./routes/posts'));
+app.use('/api/users',require('./routes/users'));
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
