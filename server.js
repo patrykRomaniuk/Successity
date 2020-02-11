c412218221d66f0d0c6403e074907ae4d680a46b
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-02-11 18:39:10 +0100

    Commented server.js

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ const app = express();
 const connectDB = require('./config/db');
 const cors = require('cors');
 
+//Connected to database
 connectDB();
 
+//Added cors
 app.use(cors());
 
+//Enabled json request
 app.use(express.json({ extended: false }));
 
+//Routes
 app.use('/api/posts',require('./routes/posts'));
 app.use('/api/users',require('./routes/users'));
 
+//Production heroku avaliable
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 
@@ -21,6 +26,8 @@ if(process.env.NODE_ENV === 'production'){
     });
 }
 
+//PORT
 const PORT = process.env.PORT || 5000;
 
+//listening to the port
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
